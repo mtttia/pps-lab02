e158db591a2d3da307a53d455327ee408a5fa2f9
mtttia <mattia.senni.03@gmail.com>
2026-03-09 10:37:14 +0100

    part2-e3b - Implementation of the neg function

--- src/main/scala/it/unibo/pps/u02/Part2.scala
+++ src/main/scala/it/unibo/pps/u02/Part2.scala
@@ -8,4 +8,14 @@ object Part2 extends App {
 
   println("The number 5 is: " + positive(5))
   println("The number -1 is: " + positive(-1))
+
+  def neg(predicate: String => Boolean)(something: String):Boolean = predicate(something) match
+    case pred if pred => false
+    case _ => true
+
+  val empty: String => Boolean = _ == ""
+  val notEmpty = neg(empty)
+  println(notEmpty("foo")) // true
+  println(notEmpty("")) // false
+  println(notEmpty("foo") && !notEmpty("")) // true.. a comprehensive test
 }
