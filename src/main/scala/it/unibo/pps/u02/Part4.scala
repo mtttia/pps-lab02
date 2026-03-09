cae5bcbfd87ac908ff8612422bab899bc72934c8
mtttia <mattia.senni.03@gmail.com>
2026-03-09 11:41:01 +0100

    part4-e8 - feature: evaluate expressions

---
+++ src/main/scala/it/unibo/pps/u02/Part4.scala
@@ -0,0 +1,15 @@
+package it.unibo.pps.u02
+
+import it.unibo.pps.u02.Part4.Expr.{Add, Literal, Multiply}
+
+object Part4 {
+  enum Expr:
+    case Literal(number:Int)
+    case Add(expr1:Expr, expr2: Expr)
+    case Multiply(expr1:Expr, expr2: Expr)
+
+  def evaluate(expr: Expr):Int = expr match
+    case Literal(number) => number
+    case Add(expr1, expr2) => evaluate(expr1) + evaluate(expr2)
+    case Multiply(expr1, expr2) => evaluate(expr1) * evaluate(expr2)
+}
---
+++ src/test/scala/task5/Part4Test.scala
@@ -0,0 +1,34 @@
+package task5
+
+import it.unibo.pps.u02.Part4.{Expr, evaluate}
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class Part4Test {
+
+  @Test def testEvaluateLiteral(): Unit = {
+    val expression: Expr = Expr.Literal(6)
+    val result: Int = 6
+    assertEquals(result, evaluate(expression))
+  }
+
+  @Test def testEvaluateAdd(): Unit = {
+    val expression:Expr = Expr.Add(Expr.Literal(6), Expr.Literal(4))
+    val result: Int = 10
+    assertEquals(result, evaluate(expression))
+  }
+
+  @Test def testEvaluateMultiply(): Unit = {
+    val expression: Expr = Expr.Multiply(Expr.Literal(6), Expr.Literal(4))
+    val result: Int = 24
+    assertEquals(result, evaluate(expression))
+  }
+
+  @Test def testEvaluateComplexExpression(): Unit = {
+    // (6+4)*5 = 50
+    val expression:Expr = Expr.Multiply(Expr.Add(Expr.Literal(6), Expr.Literal(4)), Expr.Literal(5))
+    val result:Int = 50
+    assertEquals(result, evaluate(expression))
+  }
+
+}
